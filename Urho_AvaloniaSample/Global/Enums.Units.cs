49eb60e64a1261ba640429d9f1ae8a68c639f41b
najak <najak3d@gmail.com>
2021-11-21 00:40:32 -0500

    Moved Distance.EUnits into it's own file - before changing this into a Strongly-Typed Name.

--- Urho_AvaloniaSample/Global/Enums.Units.cs
+++ Urho_AvaloniaSample/Global/Enums.Units.cs
@@ -11,25 +11,4 @@ namespace Global
     {
         Distance,   // Uses Distance.EUnit; DistanceUnit instances.
     }
-
-
-
-    public partial struct Distance
-    {
-        /// <summary>
-        /// 1:1 with "DistanceUnit.MeterMults".
-        /// "Default" is a special case. Used for test cases where data is to use the current default units,
-        /// without any conversion. Most code doesn't need to be written to handle it.
-        /// OR handle by duplicating the UnitDesc for the default?
-        /// </summary>
-        public enum EUnits
-        {
-            Default = -1,   // Equivalent to current Distance.DefaultEUnit.
-            Meters = 0,
-            Yards,
-            Feet,
-            Inches,
-            Centimeters
-        }
-    }
 }
