b5441b6f7b370de6091a2839fae1faadd9323a12
najak <najak3d@gmail.com>
2021-11-23 16:04:27 -0500

    Changed Summary comment style-- as proposal for packing more comments into fewer lines.

--- Urho_AvaloniaSample/Global/double2.cs
+++ Urho_AvaloniaSample/Global/double2.cs
@@ -4,10 +4,7 @@ using System.Text;
 
 namespace Global
 {
-    /// <summary>
-    /// "unitless" multipliers that may be different in the three axes.
-    /// See usage in Distance2D.
-    /// </summary>
+    /// <summary>Double-precision two-dimensional (XY) unitless vector value.   Same as a Vector2, but with double-precision.</summary>
     public struct double2
     {
         #region --- data ----------------------------------------
@@ -23,13 +20,8 @@ namespace Global
             Y = y;
         }
 
-        /// <summary>
-        /// For situations where the units of X and Y "cancel out".
-        /// (All that matters is their "ratio".)
-        /// This is often the case in trigonometric calculations.
-        /// Be careful to only use where valid to do so.
-        /// </summary>
-        /// <param name="pt"></param>
+        /// <summary>Converts from Distance2D, which drops the units and spatial-context.</summary>
+        /// <remarks>For situations where the units of X and Y "cancel out" (e.g. a ratio, or scalar value).</remarks>
         public double2(Distance2D pt)
         {
             X = pt.X.Value;
