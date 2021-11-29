f406ffbfc2a984b693a878444a5c3ac250f45864
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-11-28 22:41:19 -0600

    ThirdPersonPerspective = true

--- Urho_AvaloniaSample/Source/Sample.cs
+++ Urho_AvaloniaSample/Source/Sample.cs
@@ -41,7 +41,7 @@ namespace AvaloniaSample
 		Sprite logoSprite;
 		UI ui;
 
-        protected const bool ThirdPersonPerspective = false;//true;
+        protected const bool ThirdPersonPerspective = true;//true;
         protected const bool ShowTwoViewports = true;//true;   // TMS
         // Camera not only goes up as terrain rises, it also goes down as terrain falls.
         protected const bool TrackAltitude = true;
